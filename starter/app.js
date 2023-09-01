aeccf8b1b01651c0934dcc84a5130560458251c0
Mihir Aswal <montyaswal0@gmail.com>
2023-09-01 12:48:15 +0530

    api calls handled

--- starter/app.js
+++ starter/app.js
@@ -5,6 +5,8 @@ const bodyParser=require('body-parser');
 const port=8000;
 const tasks=require('./routes/task.js');
 require('./db/connect.js');
+app.use(express.static("./public"));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/v1/tasks',tasks);
